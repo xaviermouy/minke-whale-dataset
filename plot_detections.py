73e017eb0a4436b803ed6f27f688434078da6bfe
Xavier Mouy <xavier.mouy@nmfs.local>
2022-06-14 08:54:59 -0400

    brunswick

--- plot_detections.py
+++ plot_detections.py
@@ -4,6 +4,10 @@ Created on Fri Jun 10 17:34:55 2022
 
 This script creat a duirnal plot of detections or annotations
 
+# TO DO
+ - Dates/time are a bit off -> need to correct this
+
+
 @author: xavier.mouy
 """
 from ecosound.core.annotation import Annotation
@@ -11,6 +15,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mc # For the legend
+import matplotlib.dates as mdates
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import datetime as dt
+from sqlite3 import connect
+
+
 # Another utility for the legend
 from matplotlib.cm import ScalarMappable
 
@@ -23,51 +33,72 @@ def resample(data, integration_time='1H', resampler='count'):
     data_out.reset_index(drop=True,inplace=True)
     return data_out
 
-annot_file = r'C:\Users\xavier.mouy\Documents\GitHub\minke-whale-dataset\datasets\FRA-NEFSC-CARIBBEAN-201612-MTQ\Annotations_dataset_FRA-NEFSC-CARIBBEAN-201612-MTQ annotations.nc'
+## inputs #####################################################################
+#annot_file = r'C:\Users\xavier.mouy\Documents\Projects\2021_Minke_detector\results\NEFSC_CARIBBEAN_201612_MTQ\detections.sqlite'
+annot_file = r'C:\Users\xavier.mouy\Documents\Projects\2021_Minke_detector\results\NEFSC_CARIBBEAN_201612_MTQ_run1\detections.nc'
+annot_file = r'C:\Users\xavier.mouy\Documents\Projects\2021_Minke_detector\results\NEFSC_GA_201611_CH6\detections.nc'
+#annot_file = r'C:\Users\xavier.mouy\Documents\GitHub\minke-whale-dataset\datasets\FRA-NEFSC-CARIBBEAN-201612-MTQ\Annotations_dataset_FRA-NEFSC-CARIBBEAN-201612-MTQ annotations.nc'
+
 integration_time = '1H'
+is_binary = False
+norm_max = None
+
+threshold = 0.9
+###############################################################################
 
 # Load annotations
 annot = Annotation()
+
+# # manual analysis
+# annot.from_netcdf(annot_file)
+# data = annot.data
+
+# # detector 1
+# conn = connect(annot_file)
+# annot.data = pd.read_sql_query("SELECT * FROM detections", conn)
+# data = annot.data
+# data = data[data['confidence']>=threshold]
+# data['time_min_date'] = pd.to_datetime(data['time_min_date'], format="%Y-%m-%d %H:%M:%S") # Convert date
+
+# detector 2
 annot.from_netcdf(annot_file)
 data = annot.data
+data = data[data['confidence']>=threshold]
+#data['time_min_date'] = pd.to_datetime(data['time_min_date'], format="%Y-%m-%d %H:%M:%S") # Convert date
 
 # resample
 data.set_index("time_min_date", inplace=True)
 data_resamp = resample(data, integration_time=integration_time)
 
-
-# Reshape
+# Resample time series
 data_resamp['date']=data_resamp['datetime'].dt.date
 data_resamp['time']=data_resamp['datetime'].dt.time
+
+# Create 2D matrix
 axis_date = sorted(data_resamp['date'].unique())
-axis_time = sorted(data_resamp['time'].unique())
+axis_TOD = sorted(data_resamp['time'].unique()) # time of day
+axis_TOD = [dt.datetime.combine(axis_date[0], x) for x in axis_TOD]
+data_grid = pd.pivot_table(data_resamp, values='value', index='time',columns='date', aggfunc=np.sum)
+data_grid = data_grid.fillna(0) # replaces NaNs by zeros
+if is_binary:
+    data_grid[data_grid>0]=1
 
+# Plot matrix
+x_lims = mdates.date2num([axis_date[0],axis_date[-1]])
+y_lims = mdates.date2num([axis_TOD[0],axis_TOD[-1]])
+fig, ax = plt.subplots(constrained_layout=True)
+im = ax.imshow(data_grid, extent = [x_lims[0], x_lims[1],  y_lims[0], y_lims[1]], vmin=0, vmax=norm_max,aspect='auto',origin='lower',cmap='viridis')
+ax.xaxis_date()
+ax.yaxis_date()
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%b-%Y'))
+ax.yaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 
-# -> add x and y axis indices for each value of data_resamp (add x_idx, y_idx columns)
-# -> loop through data_resamp and distribute into empty matrix (start matrix with Nans)
-# -> plot matrix
-
-#gg=data_resamp.pivot_table('date','time')
-
-# # Extract hour, day, and temperature
-# hour = data_resamp["date"].dt.hour
-# day = data_resamp["date"].dt.day
-# month = data_resamp["date"].dt.month
-# year = data_resamp["date"].dt.year
-# value = data_resamp["value"]
-
-# # # Re-arrange temperature values
-# #values = value.values.reshape(24, len(day.unique()), order="F")
-# values = value.values.reshape(24, len(day), order="F")
-# # Compute x and y grids, passed to `ax.pcolormesh()`.
-
-# # The first + 1 increases the length
-# # The outer + 1 ensures days start at 1, and not at 0.
-# xgrid = np.arange(day.max() + 1) + 1
-
-# # Hours start at 0, length 2
-# ygrid = np.arange(25)
-
-# fig, ax = plt.subplots()
-# ax.pcolormesh(xgrid, ygrid, temp)
-# ax.set_frame_on(False) # remove all spines
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="2%", pad=0.05)
+cbar = plt.colorbar(im, cax=cax)
+#cbar = plt.colorbar(im, fraction=1, pad=0)
+cbar.set_label('Detections')
+fig.tight_layout()
+#plt.setp(ax.get_xticklabels(), rotation = 0)
+#fig.autofmt_xdate()
+#ax.colorbar()
